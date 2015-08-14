fabeff5497a142ad32a004daa5e34b55b8360bdc
Mani Wang <mani95lisa@gmail.com>
2015-08-14 12:02:23 +0800

    fix validator bug

--- src/Forone/Providers/ForoneValidatorProvider.php
+++ src/Forone/Providers/ForoneValidatorProvider.php
@@ -12,7 +12,12 @@ use Illuminate\Support\ServiceProvider;
 
 class ForoneValidatorProvider extends ServiceProvider {
 
-    public function boot()
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
     {
         Validator::extend('mobile', function ($attribute, $value, $parameters) {
             return preg_match("/^1[0-9]{2}[0-9]{8}$|15[0189]{1}[0-9]{8}$|189[0-9]{8}$/",$value);
@@ -22,5 +27,4 @@ class ForoneValidatorProvider extends ServiceProvider {
             return strlen($value) != 6 || !preg_match("/[0-9]{6}/", $value);
         });
     }
-
 }
